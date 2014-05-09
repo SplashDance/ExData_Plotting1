9a70f264510043a6e550c259476e23655b843053
SplashDance <SteveSargentJr@hotmail.com>
2014-05-09 14:39:01 -0400

    Added plots/changed script

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 # Plot #1  -- Histogram
 
 
-# Sets the Working Directory and loads in dataframe as "df"
+# Sets the Working Directory and loads in dataframe as "dataset"
 setwd("~/Desktop/MOOCs/Coursera/Data Science/Exploratory Data Analysis/Assignment-1")
 dataset <- read.csv('household_power_consumption.txt', header=TRUE, sep=';', 
          na.strings='?', dec='.')
@@ -18,12 +18,15 @@ dataset$Time <- strptime(dataset$Time, "%H:%M:%S")
 dataset <- within(dataset, { DateTime=format(as.POSIXct(paste(Date, Time)),
                                               "%m/%d/%Y %H:%M:%S") })
 
+# Subsets data used in analysis:
 df <- subset(dataset, (Date == "2007-02-01") | (Date == "2007-02-02"))
 
 # Create first histogram plot:
 # hist(df$Global_active_power, xlab="Global Active Power (kilowatts)", col='red',
 #      main='Global Active Power')
 
+
+par(cex=0.8)
 png(filename="plot1.png", width=480, height=480)
 with(df, 
 	hist(df$Global_active_power, xlab="Global Active Power (kilowatts)",
