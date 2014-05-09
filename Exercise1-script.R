7bf34583a62d1f2b4f43a25e65c933441359eebd
SplashDance <SteveSargentJr@hotmail.com>
2014-05-09 12:05:30 -0400

    Added 1st plot and beginning of 2nd

--- Exercise1-script.R
+++ Exercise1-script.R
@@ -5,4 +5,21 @@ setwd("~/Desktop/MOOCs/Coursera/Data Science/Exploratory Data Analysis/Assignmen
 dataset <- read.csv('household_power_consumption.txt', header=TRUE, sep=';', 
          na.strings='?', dec='.')
 
+dataset$Date <- as.Date(dataset$Date, '%d/%m/%Y')
+dataset$Time <- strptime(dataset$Time, "%H:%M:%S")
 
+# Adds a new "DateTime" column:
+dataset <- within(dataset, { DateTime=format(as.POSIXct(paste(Date, Time)),
+                                              "%m/%d/%Y %H:%M:%S") })
+
+df <- subset(dataset, (Date == "2007-02-01") | (Date == "2007-02-02"))
+
+# Create first histogram plot:
+hist(df$Global_active_power, xlab="Global Active Power (kilowatts)", col='red',
+     main='Global Active Power')
+
+# Second line plot:
+
+## NOTE: Incorrect plotting -- FIX NEEDED
+plot(df$Time, df$Global_active_power, type="l", 
+     xlab="", ylab="Global Active Power (kilowatts)")
