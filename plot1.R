3ea65b1bcb71cef53edb5df9b5226104db3cf746
SplashDance <SteveSargentJr@hotmail.com>
2014-05-09 12:47:52 -0400

    Added 1st plot

--- plot1.R
+++ plot1.R
@@ -1,12 +1,18 @@
 # R script for Project 1, Exploratory Data Analysis
+# Plot #1  -- Histogram
+
 
 # Sets the Working Directory and loads in dataframe as "df"
 setwd("~/Desktop/MOOCs/Coursera/Data Science/Exploratory Data Analysis/Assignment-1")
 dataset <- read.csv('household_power_consumption.txt', header=TRUE, sep=';', 
          na.strings='?', dec='.')
 
+
 dataset$Date <- as.Date(dataset$Date, '%d/%m/%Y')
+# dataset$Time <- strptime(dataset$Time, "%H:%M:%S")
+
 dataset$Time <- strptime(dataset$Time, "%H:%M:%S")
+
 
 # Adds a new "DateTime" column:
 dataset <- within(dataset, { DateTime=format(as.POSIXct(paste(Date, Time)),
@@ -15,11 +21,14 @@ dataset <- within(dataset, { DateTime=format(as.POSIXct(paste(Date, Time)),
 df <- subset(dataset, (Date == "2007-02-01") | (Date == "2007-02-02"))
 
 # Create first histogram plot:
-hist(df$Global_active_power, xlab="Global Active Power (kilowatts)", col='red',
-     main='Global Active Power')
+# hist(df$Global_active_power, xlab="Global Active Power (kilowatts)", col='red',
+#      main='Global Active Power')
 
-# Second line plot:
+png(filename="plot1.png", width=480, height=480)
+with(df, 
+	hist(df$Global_active_power, xlab="Global Active Power (kilowatts)",
+       col='red', main='Global Active Power'))
+dev.off()
 
-## NOTE: Incorrect plotting -- FIX NEEDED
-plot(df$Time, df$Global_active_power, type="l", 
-     xlab="", ylab="Global Active Power (kilowatts)")
+# title(main="Global Active Power", xlab="Global Active Power (kilowatts)",
+#   col="red")
