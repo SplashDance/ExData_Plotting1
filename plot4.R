78c4fbf3db83df32a543edc500f685206aed5d69
SplashDance <stevesargentjr@hotmail.com>
2014-05-10 15:32:44 -0400

    Changed plot4 file & script

--- plot4.R
+++ plot4.R
@@ -16,18 +16,19 @@ df <- read.table(datafile, skip=66637, nrow=2880, sep=";",
 # Creates new column, "DateTime", for use in plot function:
 df$DateTime <- strptime(paste(df$Date, df$Time), format='%d/%m/%Y %H:%M:%S')
 
+
 # Alters global plotting parameters:
-par(mfrow = c(2, 2), cex=0.65)
+png(filename="plot4.png", width=480, height=480)
+par(mfrow = c(2, 2))
 
-
-# Plot - Upper Left, Line plot:
+# Plot 1 - Upper Left, Line plot:
 plot(x=df$DateTime, y=df$Global_active_power, xlab="",
 	ylab='Global Active Power', type='l')
 
-# Plot - Upper Right, Line plot:
+# Plot 2 - Upper Right, Line plot:
 plot(x=df$DateTime, y=df$Voltage, type='l', ylab="Voltage", xlab='datetime')
 
-# Plot - Lower Left, Line plot:
+# Plot 3 - Lower Left, Line plot:
 plot(df$DateTime, df$Sub_metering_1, type="l", xlab="",
 	ylab="Energy sub metering")
 lines(df$DateTime, df$Sub_metering_2, type="l", xlab="", col="red")
@@ -35,9 +36,9 @@ lines(df$DateTime, df$Sub_metering_3, type="l", xlab="", col="blue")
 legend(x="topright", col=c("black", "red", "blue"), lty=1, bty='n',
 	legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-# Plot - Lower Right, Line plot:
+# Plot 4 - Lower Right, Line plot:
 plot(x=df$DateTime, y=df$Global_reactive_power, xlab='datetime',
 	ylab='Global_reactive_power', type='l')
 
-dev.copy(png, 'plot4.png', width=480, height=480)
+#dev.copy(png, 'plot4.png', width=480, height=480)
 dev.off()
